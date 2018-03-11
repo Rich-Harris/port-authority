3136800813ff0988e910284b13f0be671abc1143
Rich Harris <richard.a.harris@gmail.com>
2018-03-11 10:14:40 -0400

    start adding tests

--- test/test.ts
+++ test/test.ts
@@ -1,8 +1,47 @@
 import * as assert from 'assert';
+import * as http from 'http';
 import * as ports from '../src/index';
 
 describe('port-authority', () => {
-	// it('has tests', () => {
-	// 	assert.ok(false);
-	// });
+	function createServer(port: number): Promise<{ close: (err?: Error) => void }> {
+		return new Promise((fulfil, reject) => {
+			const server = http.createServer();
+
+			function close() {
+				return new Promise((fulfil, reject) => {
+					server.close((err?: Error) => {
+						if (err) reject(err);
+						else fulfil();
+					});
+				});
+			}
+
+			server.listen(port, (err?: Error) => {
+				if (err) reject(err);
+				else fulfil({
+					close
+				});
+			});
+		});
+	}
+
+	describe('check', () => {
+		it('returns true if a port is available', async () => {
+			assert.equal(
+				await ports.check(3000),
+				true
+			);
+		});
+
+		it('returns false if a port is unavailable', async () => {
+			const server = await createServer(3000);
+
+			assert.equal(
+				await ports.check(3000),
+				false
+			);
+
+			await server.close();
+		});
+	});
 });
